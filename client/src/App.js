74649fdcaf2b23b4a5c8820e139a8c8edbaa994b
c-yrus <Hazem0Abdennadher@gmail.com>
2022-09-17 11:18:04 +0100

    deployement

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,16 @@ function App() {
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/avatar" element={<Avatar />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="https://chatdotio.netlify.app/" element={<Chat />} />
+        <Route
+          path="https://chatdotio.netlify.app/avatar"
+          element={<Avatar />}
+        />
+        <Route
+          path="https://chatdotio.netlify.app/register"
+          element={<Register />}
+        />
+        <Route path="https://chatdotio.netlify.app/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
