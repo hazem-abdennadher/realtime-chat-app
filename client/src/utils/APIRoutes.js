f14b01c87513e1fdaaca5ad727d2681c7d3c2517
Hazem Abdennadher <hazem0abdennadher@gail.com>
2022-12-27 23:28:31 +0100

    docker

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = 'https://cyrus-realtime-chat-app.herokuapp.com';
+export const host = 'http://localhost:3001';
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/set-avatar`;
