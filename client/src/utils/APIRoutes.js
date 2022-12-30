0d935cff7180ecf5e9679e7e7edc659001405068
Hazem Abdennadher <hazemabdennadher@cyebrdivision.xyz>
2022-12-30 19:32:42 +0100

    update

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = 'http://localhost:3001';
+export const host = 'http://localhost:8081';
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/set-avatar`;
